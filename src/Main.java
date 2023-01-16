c793c16e20227e9a700d6bedb42e6a9110f745b5
VojtechTravnicek <vojtatravnicek@post.cz>
2023-01-16 22:02:41 +0100

    konec

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        seznamZkazniku zaznam = new seznamZkazniku();
+        Zakaznik zakaznik1 = new Zakaznik("Franta Novotný", 4);
+        Zakaznik zakaznik2 = new Zakaznik("Franta Novák", 8);
+        seznamZkazniku.nactiZeSouboru("vstup.txt");
+        for(Zakaznik zakaznik : zaznam.getList()){
+            System.out.println(zakaznik.getJmeno()+":"+zakaznik.getPocetProdeju()+"\n");
+        }
+        zaznam.pridat(zakaznik1);
+        zaznam.pridat(zakaznik2);
+        zaznam.zapisDoSouboru("vystup.txt");
     }
 }
